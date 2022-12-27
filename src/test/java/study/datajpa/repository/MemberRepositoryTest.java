6d1e7fa3f125e637f4d971e029a10b9f7505ae75
gustjdw <johs710@gachon.ac.kr>
2022-12-27 17:48:12 +0900

    사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -278,4 +278,9 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
